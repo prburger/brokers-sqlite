6aee97ca3f99af4f80c1adcd3d713477136b886d
Paul Burger <pburger@hotmail.com>
2020-12-31 12:39:52 +1030

    More GUI stuff - global messages

---
+++ templates/no_dashboard_records_found.php
@@ -0,0 +1,5 @@
+<tr>
+    <td colspan="4" class="no_dashboard_records_found">
+        {{'table.no_records_found'|trans|raw}}
+    </td>
+</tr>
---
+++ templates/no_records_found.php
@@ -0,0 +1,5 @@
+<tr>
+    <td  colspan="4">
+        <p class="no_records_found">{{'table.no_records_found'|trans|raw}}</p>
+    </td>
+</tr>
